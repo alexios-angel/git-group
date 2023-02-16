98e0a67e26ed0e1b27581947744553553606859b
alexiosangel <aangeletakis@gmail.com>
2023-02-16 17:24:14 -0500

    lol

--- students.py
+++ students.py
@@ -2,13 +2,14 @@ from majors import Major
 
 class Student:
     def __init__(self,student_id: int, first_name: str, last_name: str,
-                 age: int, major: Major):
+                 age: int, major: Major, status: int):
         self.student_id = student_id
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.major = major
         self.active_courses = []
+        self.status = status
 
 
     def __str__(self):
